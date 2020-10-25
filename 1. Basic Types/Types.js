ac40aee02b1786fb4bdf28b1487f8eb0b1adcc5a
Tashik Moin <tashik.moin@studentpartner.com>
2020-10-25 19:41:50 +0500

    Added basics

---
+++ 1. Basic Types/Types.js
@@ -0,0 +1,25 @@
+// PRIMITIVE
+
+// String type
+const name = 'John Doe';
+// Number type
+const age = 45;
+// Boolean type
+const hasKids = true;
+// Null type
+const car = null;
+// Undefined type
+let test;
+// Symbol type (introduced in ES6 version)
+const sym = Symbol();
+
+// REFERENCE TYPES - Objects
+
+// Array
+const hobbies = ['movies', 'music'];
+
+// Object literal
+const address = {
+    city: "Karachi",
+    state: 'Sindh'
+}
